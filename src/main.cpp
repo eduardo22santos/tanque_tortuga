49cff3115004ebe341a190444db07d02c28d2bd6
eduardo22santos <eduardo22santos@hotmail.com>
2023-04-20 16:28:46 -0300

    migrei algumas funções

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,19 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), modo, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  Serial.println(contador*60);
+  contador = 0;
+  delay(1000);
+}
+
+void modo()
+{
+  contador++;
 }
