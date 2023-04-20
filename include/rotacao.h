989f9a613e554ec6a9312bc24781e29f62cd51c9
eduardo22santos <eduardo22santos@hotmail.com>
2023-04-20 16:24:06 -0300

    primeiro commit

---
+++ include/rotacao.h
@@ -0,0 +1,5 @@
+#ifndef ROTACAO_H
+#define ROTACAO_H
+#include <Arduino.h
+
+#endif // ROTACAO_H
---
+++ src/rotacao.cpp
@@ -0,0 +1,22 @@
+#include <rotacao.h>
+
+int contador = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), modo, RISING);
+}
+
+void loop() {
+  
+  Serial.println(contador*60);
+  contador = 0;
+  delay(1000);
+}
+
+void modo()
+{
+  contador++;
+}
