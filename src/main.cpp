372eeb6ec329f8fd080388d848bcc0f2b4c85507
eduardo22santos <eduardo22santos@hotmail.com>
2023-04-22 16:53:43 -0300

    autoria

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <Arduino.h>
 #include <tensao.h>
 #include <rotacao.h>
